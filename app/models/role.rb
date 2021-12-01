a95a6ad9126ef78c6de2bfe6e4cbcec7c29d66aa
Holly Teunis <+>
2021-12-01 09:57:02 -0500

    commit tree 140fabc5c7557e8e15cd1940b9f259bb88a36f4c

---
+++ app/models/role.rb
@@ -0,0 +1,9 @@
+class Role < ActiveRecord::Base
+  has_many :auditions
+  def actors
+    self.auditions.map do |audition| 
+        audition.actor
+end
+ def location
+ end
+end
---
+++ db/migrate/seed.rb
@@ -0,0 +1,2 @@
+a1 = Audition.create({actor:"Holly", location:"Florida", phone:3017883527, hired:true, role_id:1})
+a2 = Audition.create({actor:"Max", location:"Florida", phone:3014883527, hired:true, role_id:2})
